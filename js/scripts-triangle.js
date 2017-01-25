ca5baa503500ddb2c3ad2dc23fbd522ce3fcaf59
jarek borzecki <jarek borzecki>
2017-01-25 12:57:00 +0100

    Function to calculate the area of triangle

---
+++ js/scripts-triangle.js
@@ -0,0 +1,9 @@
+// Pole trójkąta
+
+var a = prompt('Podaj długość podstawy trójkąta'),
+		h = prompt('Podaj wysokość trójkąta'),
+		triangleArea = (a * h) / 2;
+
+
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + '\n' + triangleArea);
+console.log(triangleArea);
